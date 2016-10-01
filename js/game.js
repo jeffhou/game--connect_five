3de62baa5093edbf4b0a358178d9c6b21144a890
Jeff <j.king.hou@gmail.com>
2016-10-01 05:25:32 +0000

    Added code for connect five.

--- js/game.js
+++ js/game.js
@@ -9,22 +9,96 @@ addEventListener("keyup", function (e) { delete keysDown[e.keyCode] }, false);
 var canvas = document.createElement("canvas"); //this creates the canvas! all the stuff we see exists on the canvas! woah!
 var context = canvas.getContext("2d");	//canvas specifications
 
-canvas.width = 400; canvas.height = 250;	//canvas specifications (size)
+canvas.width = 672; canvas.height = 672;	//canvas specifications (size)
 document.body.appendChild(canvas);			//place canvas in the main html code? woah?
 
 function Game () {		//sets initial game parameters? woah?
-	this.newGame();
-} Game.prototype.newGame = function () {
-    this.cards = [];
-    for (i = 0; i < 4; i++) {
-        this.cards.push(Math.floor(Math.random() * 13) + 1);
-    }
+	this.numPlayers = 2;
+	this.currentPlayer = 0;
+} Game.prototype.endTurn = function () {
+    this.currentPlayer = (this.currentPlayer + 1) % 2;
 }; var game = new Game();
 
+/**
+ * Constants singleton, collection of a lot of magic numbers
+ */
+var CONSTANTS = new function () {
+	this.hashedDirections = [-1000, -1, 1, 1000];
+	this.tileWidth = 32;
+	this.mapWidth = 21;
+	this.mapHeight = 21;
+};
 
 var IMAGES = new function () {
-	this.card = new ImageObject ("images/card.png");
+	this.empty = new ImageObject ("images/empty.png");
+	this.black = new ImageObject ("images/black.png");
+	this.white = new ImageObject ("images/white.png");
 };
+
+/**
+ * Class that contains the cursor used in the game. Self-explanatory for the
+ * most part.
+ */
+
+function Cursor() {
+	this.imageObject = new ImageObject ("images/cursor.png");
+	this.x = 10;
+	this.y = 10;
+} Cursor.prototype.coor = function () {
+	return new Coor(this.x, this.y);
+}; Cursor.prototype.draw = function () {
+	this.imageObject.drawOnGrid(this.x, this.y);
+}; Cursor.prototype.coorOnScreen = function () {
+    return this.coor().screenify();
+}; Cursor.prototype.up = function () {
+    if(cursor.y != 0) {   //if the cursor isn't in the top row
+        cursor.y -= 1;  //when you're going up, you're always decreasing the y value
+    }
+}; Cursor.prototype.down = function () {
+    if(cursor.y != 21 - 1) {
+        cursor.y += 1;
+    }
+}; Cursor.prototype.left = function () {
+    if(cursor.x != 0) {
+        cursor.x -= 1;
+    }
+}; Cursor.prototype.right = function () {
+    if(cursor.x != 21 - 1) {
+        cursor.x += 1;
+    }
+}; Cursor.prototype.jumpTo = function (coor) {
+    this.x = coor.x;
+    this.y = coor.y;
+}; cursor = new Cursor();
+
+
+/**
+ * Class that encapsulates coordinates. Screenify and unscreenify change
+ * the displacements from the top left of the screen to the top left of the
+ * entire map.
+ */
+function Coor (x, y) {
+	this.x = x;
+	this.y = y;
+} Coor.prototype.equals = function (coor) {
+	if (coor instanceof Coor) return this.x == coor.x && this.y == coor.y;
+	return false;
+}; Coor.prototype.unscreenify = function () {
+	return new Coor(this.x + grid.xDisplace, this.y + grid.yDisplace);
+}; Coor.prototype.screenify = function () {
+	return new Coor(this.x - grid.xDisplace, this.y - grid.yDisplace);
+};
+/**
+ * We hash coordinates to integers so that we can store them in arrays and
+ * use array methods without programming our own. As long as x and y are both
+ * between 0 and 999 inclusive, the coordinates and the hash are 1-to-1
+ */
+function hashCoor (coor) {
+	return coor.x * 1000 + coor.y;
+}
+function unhashCoor (hashedCoor) {
+	return new Coor(parseInt(hashedCoor / 1000), hashedCoor % 1000);
+}
 
 /**
  * ImageObject encapsulates the necessary functions to load and print an
@@ -57,20 +131,39 @@ for (i = 0; i < 21; i++) {
 }
 
 function processInputs () {
+    if (38 in keysDown) { // Player holding the up button
+        cursor.up();
+    }
+    if (40 in keysDown) { // Player holding down
+        cursor.down();
+    }
+    if (37 in keysDown) { // Player holding left
+        cursor.left();
+    }
+    if (39 in keysDown) { // Player holding right
+        cursor.right();
+    }
     if (90 in keysDown) { // pressed "z" which is actually "a" for our emulator
-        game.newGame();
+        if (grid[cursor.x][cursor.y] == -1) {
+            grid[cursor.x][cursor.y] = game.currentPlayer;
+            game.endTurn();
+        }
     }
     keysDown = {};
 }
 function drawAll () {
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
-            IMAGES.card.draw(i * 200, j * 125);
-            context.font = "28px Arial Bold";
-            context.fillStyle = "#ff00ff";
-            context.fillText(game.cards[i * 2 + j], i * 200 + 95, j * 125 + 70);
+    for (i = 0; i < 21; i++) {
+        for (j = 0; j < 21; j++) {
+            if (grid[i][j] == -1) {
+                IMAGES.empty.draw(i * 32, j * 32);
+            } else if (grid[i][j] == 0) {
+                IMAGES.black.draw(i * 32, j * 32);
+            } else if (grid[i][j] == 1) {
+                IMAGES.white.draw(i * 32, j * 32);
+            }
         }
     }
+	cursor.draw(); // draws the cursor
 };
 var main = function () {
 	processInputs();
